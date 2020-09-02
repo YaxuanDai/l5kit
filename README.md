e10ebe0e6c455e4e447d0c019f394e626dd3eb9e
代亚暄 <yaxuan.dai@outlook.com>
2020-09-02 11:17:26 +0800

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# L5Kit
+
+This is the python lib from [l5kit](https://github.com/lyft/l5kit), formed as a standard pip avaliable python package.
+
+This package could be installed by `pip install -r requirements.txt`.
+
+```
+# requirements.txt
+# Note that the `#egg=l5kit`, representing the package name, should not be removed.
+-e git+https://github.com/yaxuandai/l5kit.git#egg=l5kit
+```
+
+We use Apache 2 license for the code, following its source repo. 
+
+[License](https://github.com/YaxuanDai/l5kit)
+
+
---
+++ l5kit/geometry/angle.py
@@ -0,0 +1,47 @@
+from typing import cast
+
+import numpy as np
+
+
+def angle_between_vectors(v1: np.ndarray, v2: np.ndarray) -> float:
+    """angle_between_vectors returns the angle in radians between two vectors.
+
+    Args:
+        v1 (np.ndarray): Vector 1 of shape (N)
+        v2 (np.ndarray): Vector 2 of same shape as ``v1``
+
+    Returns:
+        float: angle in radians
+    """
+    cos_ang = np.dot(v1, v2)
+    sin_ang = np.linalg.norm(np.cross(v1, v2))
+    return cast(float, np.arctan2(sin_ang, cos_ang))
+
+
+def compute_yaw_around_north_from_direction(direction_vector: np.ndarray) -> float:
+    """compute_yaw_from_direction computes the yaw as angle between a 2D input direction vector and
+the y-axis direction vector (0, 1).
+
+    Args:
+        direction_vector (np.ndarray): Vector of shape (2,)
+
+    Returns:
+        float: angle to (0,1) vector in radians
+    """
+    return angle_between_vectors(direction_vector, np.array([0.0, 1.0]))
+
+
+def angular_distance(angle_a: float, angle_b: float) -> float:
+    """
+    Return the angular distance (angle_a - angle_b) between two angles in radians.
+    The results is always in the [-pi, pi] range
+
+    Args:
+        angle_a (float): first angle in radians
+        angle_b (float): second angle in radians
+
+    Returns:
+        angular distance in radians between two angles
+    """
+
+    return float((angle_a - angle_b + np.pi) % (2 * np.pi) - np.pi)
